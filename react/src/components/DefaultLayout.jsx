6a413a747ea43e3f1da3ae10967130c2cf979b64
Sohel Rana <sohelrananull@gmail.com>
2023-03-05 23:21:09 +0600

    usericon and usertoken

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,28 +1,41 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import { NavLink, Outlet } from 'react-router-dom'
+import { Bars3Icon, BellIcon, UserIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Navigate, NavLink, Outlet } from 'react-router-dom'
+import { UserStateContext } from '../context/ContextsProvider'
 
-const user = {
-  name: 'Tom Cook',
-  email: 'tom@example.com',
-  imageUrl:
-    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-}
+// const user = {
+//   name: 'Tom Cook',
+//   email: 'tom@example.com',
+//   imageUrl:
+//     'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+// }
 const navigation = [
   { name: 'Dashboard', to: '/' },
   { name: 'Survey', to: '/survey' },
 ]
-const userNavigation = [
-  { name: 'Login', href: '#' },
-  { name: 'Sign out', href: '#' },
-]
+// const userNavigation = [
+//   { name: 'Login', href: '#' },
+//   { name: 'Sign out', href: '#' },
+// ]
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
 export default function DefaultLayout() {
+  const {currentUser,userToken} = UserStateContext();
+  
+  if(!userToken){
+    return <Navigate to='login' />
+  }
+
+  const logout = (ev) =>{
+    ev.preventDefault();
+    console.log("logout");
+  }
+
+  
   return (
     <>
       <div className="min-h-full">
@@ -73,7 +86,7 @@ export default function DefaultLayout() {
                         <div>
                           <Menu.Button className="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                             <span className="sr-only">Open user menu</span>
-                            <img className="h-8 w-8 rounded-full" src={user.imageUrl} alt="" />
+                          <UserIcon className='w-8 h-8 bg-red-800 p-2 rounded-full text-white' />
                           </Menu.Button>
                         </div>
                         <Transition
@@ -86,21 +99,15 @@ export default function DefaultLayout() {
                           leaveTo="transform opacity-0 scale-95"
                         >
                           <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                            {userNavigation.map((item) => (
-                              <Menu.Item key={item.name}>
-                                {({ active }) => (
+                              <Menu.Item>
                                   <a
-                                    href={item.href}
-                                    className={classNames(
-                                      active ? 'bg-gray-100' : '',
-                                      'block px-4 py-2 text-sm text-gray-700'
-                                    )}
+                                    href="#"
+                                    onClick={(ev) => logout(ev)}
+                                    className={"block px-4 py-2 text-sm text-gray-700"}
                                   >
-                                    {item.name}
+                                    Sign out
                                   </a>
-                                )}
                               </Menu.Item>
-                            ))}
                           </Menu.Items>
                         </Transition>
                       </Menu>
@@ -138,11 +145,12 @@ export default function DefaultLayout() {
                 <div className="border-t border-gray-700 pt-4 pb-3">
                   <div className="flex items-center px-5">
                     <div className="flex-shrink-0">
-                      <img className="h-10 w-10 rounded-full" src={user.imageUrl} alt="" />
+                      <UserIcon className='w-8 h-8 bg-red-800 p-2 rounded-full text-white'/>
+                      {/* <img className="h-10 w-10 rounded-full" src={user.imageUrl} alt="" /> */}
                     </div>
                     <div className="ml-3">
-                      <div className="text-base font-medium leading-none text-white">{user.name}</div>
-                      <div className="text-sm font-medium leading-none text-gray-400">{user.email}</div>
+                      <div className="text-base font-medium leading-none text-white">{currentUser.name}</div>
+                      <div className="text-sm font-medium leading-none text-gray-400">{currentUser.email}</div>
                     </div>
                     <button
                       type="button"
@@ -153,16 +161,14 @@ export default function DefaultLayout() {
                     </button>
                   </div>
                   <div className="mt-3 space-y-1 px-2">
-                    {userNavigation.map((item) => (
                       <Disclosure.Button
-                        key={item.name}
                         as="a"
-                        href={item.href}
+                        href="#"
+                        onClick={(ev) => logout(ev)}
                         className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
                       >
-                        {item.name}
+                        Sign out
                       </Disclosure.Button>
-                    ))}
                   </div>
                 </div>
               </Disclosure.Panel>
--- react/src/main.jsx
+++ react/src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import App from './App'
+import { ContextsProvider } from './context/ContextsProvider'
 import './index.css'
 import router from './router.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ContextsProvider>
     <RouterProvider router={router} />
+    </ContextsProvider>
   </React.StrictMode>,
 )
